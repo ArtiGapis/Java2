d5e4fc072548f3be08aaa9d87f0d5e161ccfa4bd
Arturas <arturas.danilevicius@gmail.com>
2018-12-18 13:27:01 +0200

    juodrastis

---
+++ src/lt/bt/Java2/Juodrastis.java
@@ -0,0 +1,12 @@
+package lt.bt.Java2;
+
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.List;
+
+public class Juodrastis {
+
+    public static void main(String[] args) {
+        System.out.println("veikia");
+    }
+}
