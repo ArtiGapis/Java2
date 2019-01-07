9d2a4738791451bfb7dc6c1a9a7bb484cd2780e8
Arturas <arturas.danilevicius@gmail.com>
2019-01-07 10:13:52 +0200

    lambda pavizdys

---
+++ src/lt/bt/Java2/PVZ/Lambda.java
@@ -0,0 +1,24 @@
+package lt.bt.Java2.PVZ;
+
+public class Lambda {
+    public static void main(String[] args) {
+
+        System.out.println("=== RunnableTest ===");
+
+        // Anonymous Runnable
+
+        Runnable r1 = new Runnable(){
+            @Override
+            public void run(){
+                System.out.println("Hello world one!");
+            }
+        };
+
+        // Lambda Runnable
+         Runnable r2 = () -> System.out.println("Hello world two!");
+
+        // Run em!
+         r1.run();
+         r2.run();
+    }
+}
